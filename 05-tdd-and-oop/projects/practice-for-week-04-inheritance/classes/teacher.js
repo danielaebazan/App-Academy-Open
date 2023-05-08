0e5bd00c5f31d52629d3483b8db64fceb43fb80b
Daniela Bazán <110845822+danielaebazan@users.noreply.github.com>
2023-05-08 14:13:27 -0600

    Add files via upload

---
+++ 05-tdd-and-oop/projects/practice-for-week-04-inheritance/classes/teacher.js
@@ -0,0 +1,22 @@
+const Person = require('./person');
+
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    return teachers.reduce((total, next) => total + next.yearsOfExperience, 0);
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Teacher;
+} catch {
+  module.exports = null;
+}
