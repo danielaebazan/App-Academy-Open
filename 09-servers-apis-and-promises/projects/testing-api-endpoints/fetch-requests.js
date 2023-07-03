0ec4d4bb218a96aecfc0f6f912cd8f34b8804ad4
Daniela Bazán <110845822+danielaebazan@users.noreply.github.com>
2023-07-03 11:52:10 -0600

    Add files via upload

---
+++ 09-servers-apis-and-promises/projects/testing-api-endpoints/fetch-requests.js
@@ -0,0 +1,33 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+
+/* =============================== Phase 1 ================================ */
+/*
+  Make a request with fetch request to GET /posts and print the response
+  components to the console.
+*/
+
+fetch('/posts')
+  .then(res => res.json())
+  .then(resBody => console.log(resBody));
+
+/* =============================== Phase 2 ================================ */
+/*
+  Make a request with fetch request to POST /posts and print the response
+  components to the console.
+*/
+
+fetch('/posts', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    message: "New Post!"
+  })
+})
+  .then(res => res.json())
+  .then(resBody => console.log(resBody));
