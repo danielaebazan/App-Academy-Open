e1b86f7426d553f866af449bf9427db9d7a62690
Daniela Bazán <110845822+danielaebazan@users.noreply.github.com>
2023-07-11 15:13:54 -0600

    Add files via upload

---
+++ 10-frontend-javascript/projects/es6-modules/assets/js/index.js
@@ -0,0 +1,5 @@
+import Game from '/assets/js/game.js'
+window.onload = () => {
+    const game = new Game();
+    game.start();
+};
---
+++ 10-frontend-javascript/projects/es6-modules/assets/js/quotes/mrsPotatoHead.js
@@ -0,0 +1,5 @@
+export default {
+    "hello": "Hi, I'm Mrs. Potato Head! Pleasure to meet you.",
+    "bye": "Bye, Sweet Potato!"
+}
+
