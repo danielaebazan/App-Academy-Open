65228d53f71556b3e1bd85ff8b15120b19da137a
Daniela Bazán <110845822+danielaebazan@users.noreply.github.com>
2023-12-06 17:38:24 -0600

    Add files via upload

---
+++ 16-redux-and-authentication/projects/react-thunk-dispatch/frontend/src/components/SingleArticle/index.js
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import './SingleArticle.css';
+
+const SingleArticle = ({ articles }) => {
+  const { id } = useParams();
+
+  const singleArticle = articles.find(article => article?.id?.toString() === id);
+  return (
+    <div className='singleArticle'>
+      {singleArticle && (
+        <>
+          <h1>{singleArticle.title}</h1>
+          <img
+            src={singleArticle.imageUrl}
+            alt={singleArticle.title}
+          />
+          <p>
+            {singleArticle.body}
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default SingleArticle;
