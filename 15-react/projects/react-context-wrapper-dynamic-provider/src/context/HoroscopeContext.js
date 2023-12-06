89e772b32a86b108b1f414ac92e51ed56eef7f29
Daniela Bazán <110845822+danielaebazan@users.noreply.github.com>
2023-12-05 18:32:29 -0600

    Add files via upload

---
+++ 15-react/projects/react-context-wrapper-dynamic-provider/src/context/HoroscopeContext.js
@@ -0,0 +1,14 @@
+import { createContext, useState } from "react";
+import horoscopesObj from "../data/horoscopes";
+
+const HoroscopeProvider = (props) => {
+  const [currentSign, setCurrentSign] = useState("Aquarius");
+  const sign = horoscopesObj[currentSign];
+  return (
+    <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+};
+export const HoroscopeContext = createContext();
+export default HoroscopeProvider;
